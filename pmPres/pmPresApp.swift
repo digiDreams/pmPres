a4252af1ac0dc7ebc06e1182967c015f2256f6b6
digiDreams <digital2dreamer@gmail.com>
2023-02-27 14:19:00 +0100

    Initial Commit

---
+++ pmPres/pmPresApp.swift
@@ -0,0 +1,17 @@
+//
+//  pmPresApp.swift
+//  pmPres
+//
+//  Created by Apprenant 16 on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct pmPresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
